40092ae5bee6447214b293aca23663c7c37731ed
lennart <l.melzer@tu-bs.de>
2010-06-06 13:30:29 +0200

    Display Gravatars

--- evently/links/recent/selectors/ul/_changes/after.js
+++ evently/links/recent/selectors/ul/_changes/after.js
@@ -4,5 +4,13 @@ function(e,r) {
   $("time",widget).prettyDate();
   var message = $(".message",widget).first();
   message.html($.linkify($.mustache.escape(message.html())));
- blob.meta = e.value;
+  var authorElement = $(".author", widget).first()
+  var author = authorElement.attr("data-id");
+  $.couch.userDb(function(db) {
+      db.openDoc("org.couchdb.user:"+author, {success: function(doc) {
+        //console.debug(doc["couch.app.profile"].email);
+        authorElement.html("<img src="+doc["couch.app.profile"].gravatar_url+">");
+        }});
+  });
+  blob.meta = e.value;
 }
--- evently/links/recent/selectors/ul/_changes/data.js
+++ evently/links/recent/selectors/ul/_changes/data.js
@@ -1,6 +1,7 @@
 function(data) {
   var app = $$("#links").app, 
       profile = $$("#profile").profile, stash = data.value;
+  stash.profile_name = profile.name;
   if(profile && (stash.name == profile.name)) {
     stash.author = true;
     stash.name = "you";
