7f91c4c21fd2b7e899e192f1b878a0228b1f206e
lennart <l.melzer@tu-bs.de>
2010-07-03 10:36:46 +0200

    Filters url-shortener and filehoster from domaincloud

--- views/domains/map.js
+++ views/domains/map.js
@@ -4,7 +4,10 @@ function(doc) {
     var matches = rxp.exec(doc.url);
     var domain = matches[2];
     var parts = domain.split(".").reverse();
-    emit(parts[1]+"."+parts[0], 1);
+    var second_level_domain = parts[1]+"."+parts[0];
+    if(["bit.ly","imgur.com","yousendit.com","u.nu","tinyurl.com","j.mp","imageshack.com","yfrog.com","megaupload.com"].indexOf(second_level_domain) == -1) {
+      emit(second_level_domain, 1);
+    }
   }
 }
 
